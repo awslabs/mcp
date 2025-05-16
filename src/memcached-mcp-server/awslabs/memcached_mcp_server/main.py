5372d310c0a18e818fcba6c02f234cd83a5c19e6
seaofawareness <utkarshshah@gmail.com>
2025-05-15 17:16:07 -0700

    chore: Removed redundant log statements

--- src/memcached-mcp-server/awslabs/memcached_mcp_server/main.py
+++ src/memcached-mcp-server/awslabs/memcached_mcp_server/main.py
@@ -56,13 +56,7 @@ def main():
 
     args = parser.parse_args()
 
-    logger.trace('A trace message.')
-    logger.debug('A debug message.')
-    logger.info('An info message.')
-    logger.success('A success message.')
-    logger.warning('A warning message.')
-    logger.error('An error message.')
-    logger.critical('A critical message.')
+    logger.info('Amazon ElastiCache Memcached MCP Server Started...')
 
     # Run server with appropriate transport
     server = MemcachedMCPServer(args.sse, args.port)
