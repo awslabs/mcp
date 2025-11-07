2bbd6ccce6c7def45d3bea191a40d9d2372eccb6
Arne Wouters <awouters@amazon.com>
2025-11-07 11:08:00 +0100

    Add middleware to validate http headers

---
+++ src/aws-api-mcp-server/awslabs/aws_api_mcp_server/middleware/http_header_validation_middleware.py
@@ -0,0 +1,43 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ..core.common.config import HOST, PORT
+from fastmcp.exceptions import ClientError
+from fastmcp.server.dependencies import get_http_headers
+from fastmcp.server.middleware import Middleware, MiddlewareContext
+from loguru import logger
+
+
+class HTTPHeaderValidationMiddleware(Middleware):
+    """Validates incoming HTTP headers."""
+
+    async def on_request(
+        self,
+        context: MiddlewareContext,
+        call_next,
+    ):
+        """Validates any incoming request."""
+        headers = get_http_headers(include_all=True)
+        logger.info(headers)
+
+        if origin := headers.get('origin') or headers.get('host'):
+            logger.info('@@@ {}', origin)
+            logger.info(f'### {HOST}:{PORT}')
+            if origin != f'{HOST}:{PORT}':
+                error_msg = f'Origin validation failed: {origin} does not match {HOST}:{PORT}'
+                logger.error(error_msg)
+                raise ClientError(error_msg)
+
+        # Continue to the next middleware or handler
+        return await call_next(context)
