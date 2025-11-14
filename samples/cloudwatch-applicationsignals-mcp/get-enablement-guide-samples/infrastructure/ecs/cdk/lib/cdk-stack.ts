9a3273b12045116d9edc5025542037fcf218fa1c
wangzlei <wangzl@amazon.com>
2025-11-13 17:13:35 -0800

    fix pre-commit

--- samples/cloudwatch-applicationsignals-mcp/get-enablement-guide-samples/infrastructure/ecs/cdk/lib/cdk-stack.ts
+++ samples/cloudwatch-applicationsignals-mcp/get-enablement-guide-samples/infrastructure/ecs/cdk/lib/cdk-stack.ts
@@ -1,3 +1,17 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
@@ -160,7 +174,7 @@ export class ECSAppStack extends cdk.Stack {
     // Determine subnet configuration - prefer private subnets if available
     const privateSubnets = vpc.privateSubnets;
     const publicSubnets = vpc.publicSubnets;
-    
+
     const usePrivateSubnets = privateSubnets.length > 0;
     const subnets = usePrivateSubnets ? privateSubnets : publicSubnets;
 
